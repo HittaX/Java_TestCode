ad974583c6b73f7431c41759ea386339eecdd63e
songzp <coscomic.dota@gmail.com>
2015-07-27 01:36:36 +0800

    HashMap Test

---
+++ src/HashMapDemo.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+    public HashMap <Integer,String> cache = new HashMap<Integer,String>();
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        HashMapDemo hashMapDemo = new HashMapDemo();
+        ObjectOutputStream s1 = new ObjectOutputStream(System.out);
+
+        hashMapDemo.cache.put(1,"NewYork");
+        hashMapDemo.cache.put(2,"Tokyo");
+        hashMapDemo.cache.put(3,"Beijing");
+
+        s1.writeObject(hashMapDemo.cache);
+
+    }
+}
